38a677c62346a7b6972c989fb9f37802ddb08924
priborf2 <priborf@gmail.com>
2021-04-01 11:14:45 +0300

    Initial commit

---
+++ src/com/pribor/Main.java
@@ -0,0 +1,16 @@
+package com.pribor;
+
+/**
+ *
+ * @author Priborf
+ * @version  1 01/04/2021
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello"); //
+    //
+    }
+}
