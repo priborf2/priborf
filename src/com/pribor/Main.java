6aa7b1b5e9bf6e4eade1a4321d1b6991d7756dd6
priborf2 <priborf@gmail.com>
2021-04-02 12:03:32 +0300

    N2

--- src/com/pribor/Main.java
+++ src/com/pribor/Main.java
@@ -2,7 +2,7 @@ package com.pribor;
 
 /**
  *
- * @author Priborf
+ * @author Priborf2
  * @version  1 01/04/2021
  */
 
@@ -10,7 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello"); //
+        System.out.println("Hello"); //1
+        System.out.println("Hi"); //2
     //
     }
 }
